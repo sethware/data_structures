df975887aadd5150ab64f246ac2859f51381e06b
sethware <seth.t.eisenbraun@gmail.com>
2022-04-15 21:19:13 -0400

    Fixed flake8 warnings in Tree.py

--- src/tree/Tree.py
+++ src/tree/Tree.py
@@ -1,18 +1,22 @@
 import node.node as Node
 from dataclasses import dataclass
 
+
 @dataclass
 class Tree:
     head: Node
     size: int
-    def __init__(self,head:Node):
+
+    def __init__(self, head: Node):
         self.head = head
         self.size = 0
+
 
 def main():
     node = Node.Node()
     tree = Tree(node)
     print(f'tree: {tree}')
+
 
 if __name__ == '__main__':
     main()
