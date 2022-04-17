cb31d44164e1b2088567b0c392e64b2f313b54b5
sethware <seth.t.eisenbraun@gmail.com>
2022-04-17 16:23:03 -0400

    Added node tests and excluded venv from flake8

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,7 +1,44 @@
 from node import Node
 
 
-def test_init():
+def test_init_empty():
     n = Node()
-    print('in test')
     assert n is not None
+    assert n.value is None
+    assert n.next is None
+    assert n.left is None
+    assert n.right is None
+
+
+def test_init():
+    n = Node(1)
+    assert n is not None
+    assert n.value == 1
+    assert n.next is None
+    assert n.left is None
+    assert n.right is None
+
+
+def test_connect():
+    n1 = Node(1)
+    n2 = Node(2)
+    n1.next = n2
+    assert n1.next == n2
+    assert n2.next is None
+    assert n1.left is None
+    assert n2.left is None
+    assert n1.right is None
+    assert n2.right is None
+
+
+def test_disconnect():
+    n1 = Node(1)
+    n2 = Node(2)
+    n1.next = n2
+    n1.next = None
+    assert n1.next is None
+    assert n2.next is None
+    assert n1.left is None
+    assert n2.left is None
+    assert n1.right is None
+    assert n2.right is None
